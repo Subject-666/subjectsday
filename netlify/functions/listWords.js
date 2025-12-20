b9029042eef28e5c8d69416d19a998c54fed100c
Subject-666 <emrevemiralp@gmail.com>
2025-12-20 15:19:42 +0300

    Refactor handler function for better readability

--- netlify/functions/listWords.js
+++ netlify/functions/listWords.js
@@ -1,5 +1,9 @@
-import store from "./store.js";
+let store = global.store || {};
+global.store = store;
 
-export async function handler(){
-  return {statusCode:200, body:JSON.stringify(Object.values(store))};
+export async function handler() {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(Object.values(store))
+  };
 }
