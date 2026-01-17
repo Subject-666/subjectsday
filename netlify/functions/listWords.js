60bf53c907bf101a74848f5b85cf584a4bb6cbc7
Subject-666 <emrevemiralp@gmail.com>
2026-01-17 14:32:19 +0300

    Delete netlify/functions directory

--- netlify/functions/listWords.js
+++
@@ -1,9 +0,0 @@
-let store = global.store || {};
-global.store = store;
-
-export async function handler() {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(Object.values(store))
-  };
-}
